1df71a7fb602b3f4cf809a07cc482e21bb97e0c4
Ochaduke Biyori <pickles.ochazuke@gmail.com>
2020-03-07 16:46:08 +0900

    Game モデルの追加

---
+++ Models/Game.cs
@@ -0,0 +1,8 @@
+public class Game
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int PublicationYear { get; set; }
+    public int MinimumPlayers { get; set; }
+    public int MaximumPlayers { get; set; }
+}
